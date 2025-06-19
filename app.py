6d356bb599946de9a0a76cde6fd61b9fee16dcab
Nikhil Suresh <nikhil.suresh@encora.com>
2025-06-19 12:46:30 +0530

    changes made to the code.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from transformers import pipeline
 
 s3 = boto3.client('s3')
 local_path = 's3_download'
+model_dir = os.path.join(local_path, "bert-project-sentiment")
 s3_prefix = "bert-project-sentiment" 
 bucket_name = "bert-project-sentiment" 
 
@@ -19,7 +20,6 @@ def download_dir (local_path, s3_prefix):
         if "Contents" in result:
             for key in result["Contents"]:
                 s3_key = key['Key']
-
                 local_file = os.path.join(local_path, s3_key)
                 os.makedirs(os.path.dirname(local_file), exist_ok= True) 
                 s3.download_file(bucket_name, s3_key, local_file)
@@ -29,17 +29,22 @@ button = st.button("Download Model")
 if button:
     with st.spinner("Downloading.... Please wait!"):
         download_dir(local_path= local_path, s3_prefix= s3_prefix)
+        st.success("Download complete!")
 
 
 # 
 
 text = st.text_area("Enter your Review", "Type.....")
 predict_button = st.button ("Predict") 
-device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')  
-# "s3_download/bert-project-sentiment" 
-classifier = pipeline('text-classification', model= "tinyBert-sentiment-deployment-streamlit", device= device) 
+device = 0 if torch.cuda.is_available() else -1  
+# Predict 
+# classifier = pipeline('text-classification', model= "s3_download/bert-project-sentiment", device= device) 
 if predict_button:
-    with st.spinner("Predicting....."):
-        output = classifier(text)
-        st.write(output)
-        # st.info(output) 
+    if not os.path.exists(os.path.join(model_dir,"config.json")):
+        st.error("Model not found. Please download it first.")
+    else :
+        with st.spinner("Loading model and Predicting....."):
+            classifier = pipeline('text-classification', model=model_dir, device=device)
+            output = classifier(text)
+            st.write(output)
+            # st.info(output) 
